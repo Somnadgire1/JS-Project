7b49c7a64a4e0e2a9b71b55d9727e7b59a7863ec
Somnath_Nadgire <somnathnadgire1@gmail.com>
2022-12-09 21:32:18 +0530

    done

--- Js_Converter.js
+++ Js_Converter.js
@@ -1,120 +1,173 @@
-const fromSelected = document.getElementById("from-select");
-const toSelected = document.getElementById("to-select");
-const from = document.getElementById("from-input");
-const to = document.getElementById("to-input");
-const error = document.getElementById("error");
 
-let initialBase = "Binary",
-    lastBase = "Binary";
+function getselectedvalue(){
 
-fromSelected.addEventListener("change", function() {
-    initialBase = fromSelected.options[fromSelected.selectedIndex].text;
-    from.placeholder = initialBase + " Number";
-});
+    var selectedvalue = document.getElementById("from-input").value;
 
-toSelected.addEventListener("change", function() {
-    lastBase = toSelected.options[toSelected.selectedIndex].text;
-    to.placeholder = lastBase + " Number";
-});
+    const heading =document.querySelector(".heading");
 
-from.addEventListener("input", function() {
-    error.style.display = "none";
-});
 
-let fromValue;
-document.getElementById("convert-button").addEventListener("click", function() {
-    switch (initialBase) {
-        case "Binary":
-            fromValue = from.value;
-            if (/^[01]*$/.test(fromValue)) {
-                switch (lastBase) {
-                    case "Decimal":
-                        to.value = parseInt(fromValue, 2);
-                        break;
-                    case "Hexadecimal":
-                        to.value = parseInt(fromValue, 2).toString(16).toUpperCase();
-                        break;
-                    case "Octal":
-                        to.value = parseInt(fromValue, 2).toString(8);
-                        break;
-                    default:
-                        to.value = fromValue;
-                }
-            } else {
-                error.style.display = "inherit";
-                error.innerText = "Invalid " + initialBase + " Number";
-                to.value = "";
-            }
-            break;
+if(selectedvalue==="Octal"){
+      heading.textContent="Octal";
+}
+else if(selectedvalue==="Decimal"){
+    heading.textContent="Decimal";
+}
+else if(selectedvalue==="Hexadecimal"){
+    heading.textContent="Hexadecimal";
+}
+else{
+    heading.textContent="Binary";
+}
+}
 
-        case "Decimal":
-            fromValue = from.value;
-            if (/^[0-9]*$/.test(fromValue)) {
-                switch (lastBase) {
-                    case "Binary":
-                        to.value = Math.abs(fromValue).toString(2);
-                        break;
-                    case "Hexadecimal":
-                        to.value = Math.abs(fromValue).toString(16).toUpperCase();
-                        break;
-                    case "Octal":
-                        to.value = Math.abs(fromValue).toString(8);
-                        break;
-                    default:
-                        to.value = fromValue;
-                }
-            } else {
-                error.style.display = "inherit";
-                error.innerText = "Invalid " + initialBase +
-                    " Number";
-                to.value = "";
-            }
-            break;
+function toselectedvalue(){
 
-        case "Hexadecimal":
-            fromValue = from.value;
-            if (/^[0-9a-fA-F]*$/.test(fromValue)) {
-                switch (lastBase) {
-                    case "Binary":
-                        to.value = parseInt(fromValue, 16).toString(2);
-                        break;
-                    case "Decimal":
-                        to.value = parseInt(fromValue, 16);
-                        break;
-                    case "Octal":
-                        to.value = parseInt(fromValue, 16).toString(8);
-                        break;
-                    default:
-                        to.value = fromValue;
-                }
-            } else {
-                error.style.display = "inherit";
-                error.innerText = "Invalid " + initialBase + " Number";
-                to.value = "";
-            }
-            break;
+    var selectedvaluee = document.getElementById("to-input").value;
 
-        case "Octal":
-            fromValue = from.value;
-            if (/^[0-7]*$/.test(fromValue)) {
-                switch (lastBase) {
-                    case "Binary":
-                        to.value = parseInt(fromValue, 8).toString(2);
-                        break;
-                    case "Decimal":
-                        to.value = parseInt(fromValue, 8);
-                        break;
-                    case "Hexadecimal":
-                        to.value = parseInt(fromValue, 8).toString(16).toUpperCase();
-                        break;
-                    default:
-                        to.value = fromValue;
-                }
-            } else {
-                error.style.display = "inherit";
-                error.innerText = "Invalid " + initialBase + " Number";
-                to.value = "";
-            }
-            break;
+    const sheading =document.querySelector(".sheading");
+
+if(selectedvaluee==="Octal"){
+    sheading.textContent="To Octal no";
+}
+else if(selectedvaluee==="Decimal"){
+  sheading.textContent="To Decimal no";
+}   
+else if(selectedvaluee==="Hexadecimal"){
+  sheading.textContent="Hexadecimal";
+}
+else{
+  sheading.textContent="Binary";
+}
+}
+
+
+    const submit=document.querySelector(".submit");
+    submit.addEventListener("click", donextstep);
+
+    function donextstep(){
+    var selectedvalue = document.getElementById("from-input").value;
+    var selectedvaluee = document.getElementById("to-input").value;
+
+    if(selectedvalue==="Decimal" && selectedvaluee==="Binary"){
+    const input =Number(document.getElementById("number").value);
+    var bin = input.toString(2);
+    document.getElementById("output").value=bin;
     }
-});
+
+    else if(selectedvalue==="Decimal" && selectedvaluee==="Octal"){
+    const input =Number(document.getElementById("number").value);
+    var bin = input.toString(8);
+    document.getElementById("output").value=bin;
+    }
+    else if(selectedvalue==="Decimal" && selectedvaluee==="Hexadecimal"){
+    const input =Number(document.getElementById("number").value);
+    var bin = input.toString(16);
+    document.getElementById("output").value=bin;
+    }
+    else if(selectedvalue==="Decimal" && selectedvaluee==="Decimal"){
+    const input =Number(document.getElementById("number").value);
+    var bin = input;
+    document.getElementById("output").value=bin;
+    }
+    else if(selectedvalue==="Binary" && selectedvaluee==="Binary"){
+        const input =Number(document.getElementById("number").value);
+        var bin = input;
+        document.getElementById("output").value=bin;
+        }
+    else if(selectedvalue==="Binary" && selectedvaluee==="Octal"){
+        const input =Number(document.getElementById("number").value);
+        var bin = parseInt(input, 2).toString(8);
+        document.getElementById("output").value=bin;    
+        }
+    else if(selectedvalue==="Binary" && selectedvaluee==="Decimal"){
+        const input =Number(document.getElementById("number").value);
+        var bin =parseInt(input, 2);
+        document.getElementById("output").value=bin;    
+        }
+    else if(selectedvalue==="Binary" && selectedvaluee==="Hexadecimal"){
+        const input =Number(document.getElementById("number").value);
+        var bin =parseInt(input, 2).toString(16);
+        document.getElementById("output").value=bin;    
+        }
+        else if(selectedvalue==="Octal" && selectedvaluee==="Octal"){
+        const input =Number(document.getElementById("number").value);
+        var bin =input;
+        document.getElementById("output").value=bin;    
+        }
+        else if(selectedvalue==="Octal" && selectedvaluee==="Decimal"){
+        const input =Number(document.getElementById("number").value);
+        var bin =(parseInt(input,8));
+        document.getElementById("output").value=bin;    
+        }   
+        else if(selectedvalue==="Octal" && selectedvaluee==="Binary"){
+            const input =Number(document.getElementById("number").value);
+            var bin =(parseInt(input,8));
+            const inputt =bin;
+            var binn = inputt.toString(2);
+        document.getElementById("output").value=binn;    
+        }   
+        else if(selectedvalue==="Octal" && selectedvaluee==="Hexadecimal"){
+            const input =Number(document.getElementById("number").value);
+            var bin =(parseInt(input,8));
+            const inputt =bin
+            var binn = inputt.toString(16);
+        document.getElementById("output").value=binn;    
+        }   
+        else if(selectedvalue==="Hexadecimal" && selectedvaluee==="Hexadecimal"){
+            const input =Number(document.getElementById("number").value);
+            var bin =input;
+            document.getElementById("output").value=bin;    
+            }  
+        else if(selectedvalue==="Hexadecimal" && selectedvaluee==="Octal"){
+            const input =document.getElementById("number").value;
+            var din =parseInt(input,16);
+            var bin = din.toString(8);
+            document.getElementById("output").value=bin;  
+            }  
+        else if(selectedvalue==="Hexadecimal" && selectedvaluee==="Decimal"){
+            const input =document.getElementById("number").value;
+            var din =parseInt(input,16);
+            document.getElementById("output").value=din;  
+            } 
+        else if(selectedvalue==="Hexadecimal" && selectedvaluee==="binary"){
+            const input =document.getElementById("number").value;
+            var din =parseInt(input,16);
+            var bin = din.toString(2);
+            document.getElementById("output").value=bin;  
+             }  else {
+                error.style.display = "inherit";
+                error.innerText = "Invalid " + initialBase + " Number";
+                to.value = "";
+            }
+        }   
+
+// ----------reset---------
+const reset=document.querySelector(".reset");
+reset.addEventListener("click", clearall);
+
+function clearall(){
+    const input = document.getElementById("number");
+    const output = document.getElementById("output");
+   input.value = '';
+   output.value = '';
+
+}
+
+// --------Swapping------
+const swap=document.querySelector(".swap");
+swap.addEventListener("click",swapfunc);
+
+function swapfunc(){
+    var selectedvalue = document.getElementById("from-input").value;
+    var selectedvaluee = document.getElementById("to-input").value;
+
+var fse=document.getElementById("from-input");
+var sse=document.getElementById("to-input");
+
+fse.value=selectedvaluee;
+sse.value=selectedvalue;
+
+getselectedvalue();
+toselectedvalue();
+
+}
